5ba0554ab13e50812db20b5eeebc87518c926363
Martijn Jacobs <martijn@devopsconsulting.nl>
2016-09-28 11:47:01 +0200

    Fix clouseau to be hardcoded locally

--- release.sh
+++ release.sh
@@ -11,6 +11,7 @@ sed -i'' 's/data_dir, ".\/data"/data_dir, "\/usr\/local\/couchdb\/data"/' ./rel/
 sed -i'' 's/view_index_dir, ".\/data"/view_index_dir, "\/usr\/local\/couchdb\/data"/' ./rel/couchdb.config
 sed -i'' 's/log_file, ""/log_file, "\/usr\/local\/couchdb\/var\/log\/couchdb.log"/' ./rel/couchdb.config
 sed -i'' 's/fauxton_root, ".\/share\/www"/fauxton_root, "\/usr\/local\/couchdb\/share\/www"/' ./rel/couchdb.config
+sed -i'' 's/{{clouseau_name}}/clouseau1@127.0.0.0/' ./rel/overlay/etc/local.ini
 
 # build it
 make release
